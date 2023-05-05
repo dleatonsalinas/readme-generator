bcf2df5b659a5f70422dca751607ab535312c8e9
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-05 13:14:49 -0500

    update generatemarkdown.js with user input sections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,9 +2,20 @@
 // If there is no license, return an empty string
 const renderLicenseBadge = license => license ? `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)\n` : '';
 
-//Create table of contents
+//Create table of contents and sections based on user input
 const createTableOfContents = contentsArr => contentsArr.map(item => `- [${item}](#${item.toLowerCase().replace(/\s+/g, '-')})`).join('\n');
 
+const createInstallation = install => install ? `To use this application, please install: \n\`\`\`\n${install}\n\`\`\`` : '';
+
+const createDescription = (title, description, link) => link ? `${description}\n\nView the deployed page at [${title}](${link}).` : `${description}`;
+
+const createBuiltWith = builtWith => builtWith ? builtWith.map(item => `* ${item}`).join('\n') : '';
+
+const createLicense = license => license ? `This application is licensed under the ${license} license.` : '';
+
+const createTest = test => test ? `To run tests on the application, install\n\`\`\`\n${test}\n\`\`\`\nand run \`npm run test\` from the command line.` : '';
+
+const createQuestions = (email, github, repo) => email ? `If you have any questions about the repo, please [open an issue](https://github.com/${github}/${repo}/issues) or contact me via email at ${email}. You can find more of my work on my GitHub, [${github}](https://github.com/${github}/).` : '';
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
