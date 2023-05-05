f99398a2d843bcf69b585c925d3c997f23b35e42
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-05 13:11:15 -0500

    add license badge and table of contents to generatemarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = license => license ? `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)\n` : '';
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//Create table of contents
+const createTableOfContents = contentsArr => contentsArr.map(item => `- [${item}](#${item.toLowerCase().replace(/\s+/g, '-')})`).join('\n');
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
