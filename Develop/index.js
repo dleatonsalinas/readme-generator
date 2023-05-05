ae98b180bbf9072c633a1a607157af38eaea761e
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-05 08:29:06 -0500

    begin adding questions for user input to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,94 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Please enter the name of your repo.',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your repo.')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your application.',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please enter a description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide information for using your application.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide information for using your application.');
+                return false;
+            }
+        }
+    },
+//TODO: add checkbox contents
+    {
+    type: 'input',
+        name: 'link',
+        message: 'Please provide a link to your deployed application.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Deployed Application') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('Please enter a link.');
+                return false;
+            }
+        }
+    },
+//TODO: add installation, license, built with, contributing, tests, contact info, *screenshot, *credit
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
